2cf5f672eaa4840db861be5cca09c8a51991337d
Dhananjay <your-github-email@gmail.com>
2026-06-28 19:24:52 +0530

    9th update

--- Frontend/src/services/api.js
+++ Frontend/src/services/api.js
@@ -3,6 +3,9 @@ import axios from "axios";
 const api = axios.create({
   baseURL: "https://razorpay-assignment-v52p.onrender.com/rest",
   withCredentials: true,
+  headers: {
+    "Content-Type": "application/json",
+  },
 });
 
 
